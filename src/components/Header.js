45fbe7b26f599cd88232ae634e6ef46d9a891a25
Mauricio Blum <mauriciocblum@gmail.com>
2019-03-05 23:43:09 -0300

    Added react components and finished styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./css/header.scss";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Header</h1>
+      <div className="header">
+        <h3>Blumbook</h3>
       </div>
     );
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import PostHeader from "./PostHeader";
 
+import "./css/post.scss";
+
 export default class Post extends Component {
   render() {
     return (
-      <div>
-        <PostHeader />
-        <h3>Posts</h3>
+      <div className="container">
+        {this.props.data.map(post => (
+          <div className="post">
+            <PostHeader author={post.author} time={post.time} img={post.img} />
+            <div className="post-text">
+              <p>{post.content}</p>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
